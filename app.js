fe00dc526eafb7ae06018e70706d540d1a86bf30
Zeeshan Islam <159948421+Rohaan001@users.noreply.github.com>
2024-12-06 07:50:59 +0500

    add

---
+++ app.js
@@ -0,0 +1,81 @@
+
+// JS Assignment 23
+
+// Question 01
+
+// var itemsArray = [
+//     { name: "juice", price: 50, quantity: 3 },
+//     { name: "cookie", price: 30, quantity: 9 },
+//     { name: "shirt", price: 880, quantity: 1 },
+//     { name: "pen", price: 100, quantity: 2 }
+// ];
+
+// var totalPrice = 0;
+
+// for (var i = 0; i < itemsArray.length; i++) {
+//     var item = itemsArray[i];
+
+//     var itemTotal = item.price * item.quantity;
+//     console.log("Total price for " + item.name + ": " + itemTotal);
+
+//     totalPrice += itemTotal;
+// }
+
+// console.log("Total price of all items: " + totalPrice);
+
+// Question 02
+
+// var user = {
+//     name: "John Doe",
+//     email: "john.doe@example.com",
+//     password: "password123",
+//     age: 30,
+//     gender: "Male",
+//     city: "New York",
+//     country: "USA"
+// };
+
+// if ("age" in user) {
+//     console.log("The 'age' property exists.");
+// } else {
+//     console.log("The 'age' property does not exist.");
+// }
+
+// if ("country" in user) {
+//     console.log("The 'country' property exists.");
+// } else {
+//     console.log("The 'country' property does not exist.");
+// }
+
+// if ("firstName" in user) {
+//     console.log("The 'firstName' property exists.");
+// } else {
+//     console.log("The 'firstName' property does not exist.");
+// }
+
+// if ("lastName" in user) {
+//     console.log("The 'lastName' property exists.");
+// } else {
+//     console.log("The 'lastName' property does not exist.");
+// }
+
+// Question 03
+
+// function Person(name, email, age, gender, city, country) {
+//     this.name = name;
+//     this.email = email;
+//     this.age = age;
+//     this.gender = gender;
+//     this.city = city;
+//     this.country = country;
+// }
+
+// var person1 = new Person("John Doe", "john.doe@example.com", 30, "Male", "New York", "USA");
+// var person2 = new Person("Jane Smith", "jane.smith@example.com", 25, "Female", "Los Angeles", "USA");
+// var person3 = new Person("Sam Brown", "sam.brown@example.com", 35, "Male", "Chicago", "USA");
+
+// console.log(person1);
+// console.log(person2);
+// console.log(person3);
+
+// Question 04
